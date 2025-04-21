96e9ebe393fd1912c4fd314ebf367680fe4842da
Florent Roques <florentroques@users.noreply.github.com>
2025-04-21 17:17:04 +0200

    Update greetings.js

--- vocabulary/greetings.js
+++ vocabulary/greetings.js
@@ -1,16 +1,16 @@
 window.greetings = [
     // Salutations de base
-    { japanese: "konnichiwa", french: "bonjour", politeness: "poli" },
-    { japanese: "ohayou gozaimasu", french: "bonjour (matin)", politeness: "poli" },
-    { japanese: "konbanwa", french: "bonsoir", politeness: "poli" },
+    { japanese: "konnichiwa", french: ["bonjour"], politeness: "poli" },
+    { japanese: "ohayou gozaimasu", french: ["bonjour (matin)"], politeness: "poli", direction: "frenchToJapanese" },
+    { japanese: "konbanwa", french: ["bonsoir"], politeness: "poli" },
     
     // Salutations de départ
-    { japanese: "sayounara", french: "au revoir", politeness: "poli" },
-    { japanese: "mata ashita", french: "à demain", politeness: "poli" },
-    { japanese: "oyasuminasai", french: "bonne nuit", politeness: "poli" },
+    { japanese: "sayounara", french: ["au revoir"], politeness: "poli" },
+    { japanese: "mata ashita", french: ["à demain"], politeness: "poli" },
+    { japanese: "oyasuminasai", french: ["bonne nuit"], politeness: "poli" },
     
     // Salutations de politesse
-    { japanese: "hajimemashite", french: "enchanté", politeness: "poli" },
-    { japanese: "douzo yoroshiku", french: "ravi de faire votre connaissance", politeness: "poli" },
-    { japanese: "yoroshiku onegai shimasu", french: "formule de politesse à la fin d'un échange", politeness: "très poli" }
+    { japanese: "hajimemashite", french: ["enchanté", "enchantée"], politeness: "poli" },
+    { japanese: "douzo yoroshiku", french: ["ravi de faire votre connaissance", "ravie de faire votre connaissance"], politeness: "poli" },
+    { japanese: "yoroshiku onegai shimasu", french: ["formule de politesse à la fin d'un échange"], politeness: "très poli" }
 ]; 
